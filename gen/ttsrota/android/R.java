048fee897fb80da7b9fe93f7e32ea6a77c685935
Adriano Flach <flachadriano@gmail.com>
2011-12-28 23:15:08 -0200

    tela inicial

--- gen/ttsrota/android/R.java
+++ gen/ttsrota/android/R.java
@@ -13,6 +13,15 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int destino=0x7f050003;
+        public static final int labelDestino=0x7f050002;
+        public static final int labelOrigem=0x7f050000;
+        public static final int labelRota=0x7f050005;
+        public static final int origem=0x7f050001;
+        public static final int rota=0x7f050006;
+        public static final int solicitarRota=0x7f050004;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
