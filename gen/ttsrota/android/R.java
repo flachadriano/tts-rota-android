75ec96a91ec1423c714049254270319a81a1e15a
Adriano Flach <flachadriano@gmail.com>
2011-12-31 00:26:12 -0200

    solicitando rota

--- gen/ttsrota/android/R.java
+++ gen/ttsrota/android/R.java
@@ -8,21 +8,24 @@
 package ttsrota.android;
 
 public final class R {
+    public static final class array {
+        public static final int locomocao_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int destino=0x7f050003;
-        public static final int labelDestino=0x7f050002;
-        public static final int labelLocomocao=0x7f050004;
-        public static final int labelOrigem=0x7f050000;
-        public static final int labelRota=0x7f050007;
-        public static final int locomocao=0x7f050005;
-        public static final int origem=0x7f050001;
-        public static final int rota=0x7f050008;
-        public static final int solicitarRota=0x7f050006;
+        public static final int destino=0x7f060003;
+        public static final int labelDestino=0x7f060002;
+        public static final int labelLocomocao=0x7f060004;
+        public static final int labelOrigem=0x7f060000;
+        public static final int labelRota=0x7f060007;
+        public static final int locomocao=0x7f060005;
+        public static final int origem=0x7f060001;
+        public static final int rota=0x7f060008;
+        public static final int solicitarRota=0x7f060006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/ttsrota/android/TTSRotaActivity.java
+++ src/ttsrota/android/TTSRotaActivity.java
@@ -1,13 +1,58 @@
 package ttsrota.android;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class TTSRotaActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// carregando as opcoes de locomocao
+		Spinner locomocao = (Spinner) findViewById(R.id.locomocao);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.locomocao_array,
+				android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		locomocao.setAdapter(adapter);
+
+		// solicitar rota
+		final Button solicitarRota = (Button) findViewById(R.id.solicitarRota);
+		solicitarRota.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				try {
+					HttpClient hc = new DefaultHttpClient();
+					HttpPost post = new HttpPost("http://maps.google.com.br/maps/api/directions/json?origin=Rua José Kasteler, Jaraguá Esquerdo, Jaraguá do Sul - Santa Catarina&destination=Rua Arduino Pradi, São Luis, Jaraguá do Sul - Santa Catarina&language=pt-BR&sensor=false");
+
+					HttpResponse rp = hc.execute(post);
+
+					String rota = "";
+					if (rp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+						rota = EntityUtils.toString(rp.getEntity());
+					}
+					EditText rotaEdit = (EditText) findViewById(R.id.rota);
+					rotaEdit.setText(rota);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
